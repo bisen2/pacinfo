0fbb77bf847fff7664a7f4f38d8d5fbe77800fea
Ben Isenhart <bisenhar@uvm.edu>
2018-11-09 16:29:55 -0500

    not sure, found this newer version

--- cpacinfo.c
+++ cpacinfo.c
@@ -3,6 +3,11 @@
 /* I am also learning ncurses while writing this, so pardon any studpidity */
 /* Author: bisen2 <bisenhar@uvm.edu> */
 
+/*
+ * Potentially use the windows library to implement a ranger-type ui?
+ * 	need to read up on windows a bit more first
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,6 +15,8 @@
 #include <menu.h>
 
 #define STR_LEN 100
+
+MENU* menu_creator(ITEM*, ITEM**);
 
 int main(void) {
 
@@ -47,59 +54,91 @@ int main(void) {
 	strcat(str_expl_aur, expl_aur);
 
 	/* make menu and items */
-	ITEM **items;
-	MENU *item_menu;
+	ITEM **choices;
+	MENU *choices_menu;
+	MENU *packages_menu;
 	ITEM *cur_item;
-	items = (ITEM **) calloc(6, sizeof(ITEM *));
-	items[0] = new_item(str_all, "");
-	items[1] = new_item(str_expl, "");
-	items[2] = new_item(str_all_off, "");
-	items[3] = new_item(str_all_aur, "");
-	items[4] = new_item(str_expl_off, "");
-	items[5] = new_item(str_expl_aur, "");
-	item_menu = new_menu((ITEM **)items);
-	post_menu(item_menu);
+	choices = (ITEM **) calloc(6, sizeof(ITEM *));
+	choices[0] = new_item(str_all, "");
+	choices[1] = new_item(str_expl, "");
+	choices[2] = new_item(str_all_off, "");
+	choices[3] = new_item(str_all_aur, "");
+	choices[4] = new_item(str_expl_off, "");
+	choices[5] = new_item(str_expl_aur, "");
+	choices_menu = new_menu((ITEM **)choices);
+	post_menu(choices_menu);
 	refresh();
 
 	/* allow user to move through menu, or exit */
 	while ((c=getch()) != 'q') {
 		switch(c) {
 			case KEY_DOWN:
-				menu_driver(item_menu, REQ_DOWN_ITEM);
+			case 'j':
+				menu_driver(choices_menu, REQ_DOWN_ITEM);
 				break;
 			case KEY_UP:
-				menu_driver(item_menu, REQ_UP_ITEM);
-				break;
 			case 'k':
-				menu_driver(item_menu, REQ_UP_ITEM);
+				menu_driver(choices_menu, REQ_UP_ITEM);
 				break;
-			case 'j':
-				menu_driver(item_menu, REQ_DOWN_ITEM);
+			case KEY_ENTER:
+			case 'l':
+				/* call menu creator */
+				packages_menu = menu_creator(cur_item, choices);
+				/* move user to packages menu */
+				unpost_menu(choices_menu);
+				post_menu(packages_menu);
+				refresh();
 				break;
-//			case 'l':
-//			case 10:	/* Enter */
-//				cur_item = current_item(item_menu);
-//				clear();
-//				/* go to info about subset of packages */
-//				refresh();
-//				break;
-//			case 'h':
-//				clear();
-//				post_menu(item_menu); /* return to main menu */
-//				refresh();
-//				break;
+			case 'h':
+				/* return to main menu */
+				unpost_menu(packages_menu);
+				post_menu(choices_menu);
+				break;
 		}
 	}
 
 	/* restore terminal and exit gracefully */
-	free_item(items[0]);
-	free_item(items[1]);
-	free_item(items[2]);
-	free_item(items[3]);
-	free_item(items[4]);
-	free_item(items[5]);
-	free_menu(item_menu);
+	free_item(choices[0]);
+	free_item(choices[1]);
+	free_item(choices[2]);
+	free_item(choices[3]);
+	free_item(choices[4]);
+	free_item(choices[5]);
+	free_menu(choices_menu);
 	endwin();
 	return 0;
 
+}
+
+MENU* menu_creator(ITEM *cur_item, ITEM **choices) {
+	/* menu creator function */
+	/* make empty menu */
+	MENU *packages_menu;
+	int num_packages;
+	/* determine which menu to create and get number of packages */
+	if (cur_item == choices[0]) {
+		/* all packages */
+		num_packages = 500;
+	} else if (cur_item == choices[1]) {
+		/* explicitly installed */
+		num_packages = 100;
+	} else if (cur_item == choices[2]) {
+		/* all from official */
+		num_packages = 300;
+	} else if (cur_item == choices[3]) {
+		/* all from AUR */
+		num_packages = 200;
+	} else if (cur_item == choices[4]) {
+		/* explicit from official */
+		num_packages = 70;
+	} else if (cur_item == choices[5]) {
+		/* explicit from AUR */
+		num_packages = 30;
+	} else { printw("Oops, you appear to have found a bug.\nPlease report to bisenhar@uvm.edu"); }
+	/* make items for menu */
+	ITEM **packages;
+	for (int i=0; i<num_packages-1; i++) {
+		/* make num_packages items for menu */
+		printw("%d",i);
+	}
 }
