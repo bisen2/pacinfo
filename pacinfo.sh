955f46dbd8ab0ee3717f8e13f709a27474e51257
Ben Isenhart <bisenhar@uvm.edu>
2018-11-21 10:38:16 -0500

    add speed branch with PID script

--- pacinfo.sh
+++ pacinfo.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 # script to provide info about installed packages
-echo "All Packages: $(pacman -Q | wc -l)"
-echo "Explicitly Installed: $(pacman -Qe | wc -l)"
-echo "Explicitly Installed from Official Repos: $(pacman -Qne | wc -l)"
-echo "Total Installed from Official Repos: $(pacman -Qn | wc -l)"
+echo "All Packages: $(pacman -Q | wc -l)" & PIDA=$!
+echo "Explicitly Installed: $(pacman -Qe | wc -l)" & PIDE=$!
+wait $PIDA
+wait $PIDE
+echo "Explicitly Installed from Official Repos: $(pacman -Qne | wc -l)" & PIDEO=$!
+echo "Total Installed from Official Repos: $(pacman -Qn | wc -l)" & PIDO=$!
+wait $PIDEO
+wait $PIDO
 echo "Installed from AUR: $(pacman -Qm | wc -l)"
