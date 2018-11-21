b813001c124953546c0bb029b0333ec13ed49c43
Ben Isenhart <bisenhar@uvm.edu>
2018-11-21 10:27:26 -0500

    Remove explicit vs total for AUR

--- pacinfo.sh
+++ pacinfo.sh
@@ -2,11 +2,6 @@
 # script to provide info about installed packages
 echo "All Packages: $(pacman -Q | wc -l)"
 echo "Explicitly Installed: $(pacman -Qe | wc -l)"
-echo "Explicitly Installed from Official Repos: $(pacman -Qne | wc -l)" & PIDEO=$!
-echo "Total Installed from Official Repos: $(pacman -Qn | wc -l)" & PIDO=$!
-wait $PIDEO
-wait $PIDO
-echo "Explicitly Installed from AUR: $(pacman -Qme | wc -l)" & PIDAO=$!
-echo "Total Installed from AUR: $(pacman -Qm | wc -l)" & PIDA=$!
-wait $PIDAO
-wait $PIDA
+echo "Explicitly Installed from Official Repos: $(pacman -Qne | wc -l)"
+echo "Total Installed from Official Repos: $(pacman -Qn | wc -l)"
+echo "Installed from AUR: $(pacman -Qm | wc -l)"
